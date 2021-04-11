4457a582649118878f0d75972bcca61cdf960713
vince-prof <vincenzo.dibella@salesianiverona.it>
2021-04-11 18:41:21 +0200

    backup

--- Ops.java
+++ Ops.java
@@ -11,7 +11,7 @@ public class Operazioni {
 	*/
    public static void main(String[] args) {
       System.out.println("Ciao, tutti!\n");
-	  int result = sum(5,6);
+	  int result = somma(5,6);
 	  int result2 = prodotto(5,6);
 	  int result3 = prodotto(5,8);
 	  System.out.println("la somma di 5 + 6 e' "+ result);
@@ -20,7 +20,7 @@ public class Operazioni {
      *@param b secondo numero da sommare
      *@return restituisce la somma di due numeri in input
 	 */
-   public static int sum(int a, int b){
+   public static int somma(int a, int b){
 	return (a+b);
    }
    
